6113bfaa2bd501b422f7690416a508745f437ada
manpreetbahtra <116598899+manpreetbahtra@users.noreply.github.com>
2023-10-06 18:13:44 +0100

    Add files via upload

---
+++ sumOfPrimes.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<math.h>
+
+
+int is_prime(int num) {
+    if (num <= 1) {
+        return 0;
+    }
+    for (int i = 2; i * i <= num; i++) {
+        if (num % i == 0) {
+            return 0;
+        }
+    }
+    return 1;
+}
+
+int sumOfPrimes(int n) {
+    int sum = 0;
+    for (int i = 2; i <= n; i++) {
+        if (is_prime(i)) {
+            sum += i;
+        }
+    }
+    return sum;
+}
+
+
+
+
+
