146e41a1257564f96d0721e19419cac9b70768fb
manpreetbahtra <116598899+manpreetbahtra@users.noreply.github.com>
2023-10-06 10:49:10 +0100

    Add files via upload

---
+++ lab1Exercise2.c
@@ -0,0 +1,24 @@
+//Exercise 2 Lab 1 - swap function in C 
+
+#include <stdio.h>
+
+// write the code for the function swap() that would swap the values of two integers
+
+void swap(int *val_a, int *val_b){
+    int temp;
+    temp = *val_a;
+    *val_a=*val_b;
+    *val_b=temp;
+}
+
+int main(int argc, char *argv[]) {
+   int val_a = 50;
+   int val_b = 20;
+   // call the swap() function correctly, swap() should swap the values stored in val_a and val_b so
+   // that the swapped values of val_a and val_b are printed in the next two lines
+    swap(&val_a, &val_b);
+
+   printf("val_a is %d (should be 20)\n", val_a);
+   printf("val_b is %d (should be 50)\n", val_b);
+  return 0;
+}
